036108b31fcb9fe0ffe492e7a647ec0960a69f23
AryanVerma4455 <aryanv4455@gmail.com>
2025-03-25 00:10:54 +0530

    Add files via upload
    The system detect the lpg around if it concentration is high it trigger the buzzer and red led if the concentration of gas is low or negelcgible it turned on green led !!

---
+++ Gas_detection.ino
@@ -0,0 +1,103 @@
+﻿int gasSensorPin = A0;    
+int redLedPin = 9;         
+int greenLedPin = 8;       
+int buzzerPin = 10;        
+int gasThreshold = 400;   
+
+void setup() {
+    Serial.begin(9600);
+
+    pinMode(redLedPin, OUTPUT);
+    pinMode(greenLedPin, OUTPUT);
+    pinMode(buzzerPin, OUTPUT);
+    pinMode(gasSensorPin, INPUT);
+}
+
+void loop() {
+    int gasValue = analogRead(gasSensorPin);
+
+    Serial.print("gas_value = "int gasSensorPin = A0;    
+int redLedPin = 9;         
+int greenLedPin = 8;       
+int buzzerPin = 10;        
+int gasThreshold =500 ;   
+
+void setup() {
+    Serial.begin(9600);
+
+    pinMode(redLedPin, OUTPUT);
+    pinMode(greenLedPin, OUTPUT);
+    pinMode(buzzerPin, OUTPUT);
+    pinMode(gasSensorPin, INPUT);
+}
+
+void loop() {
+    int gasValue =int gasSensorPin = A0;    
+int redLedPin = 9;         
+int greenLedPin = 8;       
+int buzzerPin = 10;        
+int gasThreshold = 450;   
+
+void setup() {
+    Serial.begin(9600);
+
+    pinMode(redLedPin, OUTPUT);
+    pinMode(greenLedPin, OUTPUT);
+    pinMode(buzzerPin, OUTPUT);
+    pinMode(gasSensorPin, INPUT);
+}
+
+void loop() {
+    int gasValue = analogRead(gasSensorPin);
+
+    Serial.print("gas_value = "int gasSensorPin = A0;    
+int redLedPin = 9;         
+int greenLedPin = 8;       
+int buzzerPin = 10;        
+int gasThreshold =500 ;   
+
+void setup() {
+    Serial.begin(9600);
+
+    pinMode(redLedPin, OUTPUT);
+    pinMode(greenLedPin, OUTPUT);
+    pinMode(buzzerPin, OUTPUT);
+    pinMode(gasSensorPin, INPUT);
+}
+
+void loop() {
+    int gasValue = analogRead(gasSensorPin);
+
+    Serial.print("gas_value = ");
+    Serial.println(gasValue);
+
+    if (gasValue > gasThreshold) {
+        Serial.println("WARNING GAS LEAK");
+        tone(buzzerPin, 1000);
+        digitalWrite(redLedPin, HIGH);
+        digitalWrite(greenLedPin, LOW);
+    } else {
+        noTone(buzzerPin);
+        digitalWrite(redLedPin, LOW);
+        digitalWrite(greenLedPin, HIGH);
+    }
+
+    delay(1000);
+} analogRead(gasSensorPin);
+
+    Serial.print("gas_value = ");
+    Serial.println(gasValue);
+
+    if (gasValue > gasThreshold) {
+        Serial.println("WARNING GAS LEAK");
+        tone(buzzerPin, 1000);
+        digitalWrite(redLedPin, HIGH);
+        digitalWrite(greenLedPin, LOW);
+    } else {
+        noTone(buzzerPin);
+        digitalWrite(redLedPin, LOW);
+        digitalWrite(greenLedPin, HIGH);
+    }
+
+    delay(1000);
+}
