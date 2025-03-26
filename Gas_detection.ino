54f768f239f19862b89b0a76b18e4afcfbd732a0
AryanVerma4455 <aryanv4455@gmail.com>
2025-03-26 11:32:49 +0530

    Update Gas_detection.ino

--- Gas_detection.ino
+++ Gas_detection.ino
@@ -1,103 +1,33 @@
-﻿int gasSensorPin = A0;    
-int redLedPin = 9;         
-int greenLedPin = 8;       
-int buzzerPin = 10;        
-int gasThreshold = 400;   
+﻿int gasSensor = A0;   
+int buzzer = 10;      
+int redLED = 9;      
+int greenLED = 8;    
+int threshold = 400;
 
 void setup() {
-    Serial.begin(9600);
-
-    pinMode(redLedPin, OUTPUT);
-    pinMode(greenLedPin, OUTPUT);
-    pinMode(buzzerPin, OUTPUT);
-    pinMode(gasSensorPin, INPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(redLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+  pinMode(gasSensor,INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-    int gasValue = analogRead(gasSensorPin);
+  int gasLevel = analogRead(gasSensor);
+  Serial.print("Gas Level:   ");
+  Serial.println(gasLevel);
 
-    Serial.print("gas_value = "int gasSensorPin = A0;    
-int redLedPin = 9;         
-int greenLedPin = 8;       
-int buzzerPin = 10;        
-int gasThreshold =500 ;   
+  if (gasLevel > threshold) { 
+    tone(buzzer, 1000); 
+    digitalWrite(redLED, HIGH); 
+    digitalWrite(greenLED, LOW); 
+    Serial.println("WARNING!");
 
-void setup() {
-    Serial.begin(9600);
+  } else {
+    noTone(buzzer); 
+    digitalWrite(redLED, LOW);
+    digitalWrite(greenLED, HIGH); 
+  }
 
-    pinMode(redLedPin, OUTPUT);
-    pinMode(greenLedPin, OUTPUT);
-    pinMode(buzzerPin, OUTPUT);
-    pinMode(gasSensorPin, INPUT);
-}
-
-void loop() {
-    int gasValue =int gasSensorPin = A0;    
-int redLedPin = 9;         
-int greenLedPin = 8;       
-int buzzerPin = 10;        
-int gasThreshold = 450;   
-
-void setup() {
-    Serial.begin(9600);
-
-    pinMode(redLedPin, OUTPUT);
-    pinMode(greenLedPin, OUTPUT);
-    pinMode(buzzerPin, OUTPUT);
-    pinMode(gasSensorPin, INPUT);
-}
-
-void loop() {
-    int gasValue = analogRead(gasSensorPin);
-
-    Serial.print("gas_value = "int gasSensorPin = A0;    
-int redLedPin = 9;         
-int greenLedPin = 8;       
-int buzzerPin = 10;        
-int gasThreshold =500 ;   
-
-void setup() {
-    Serial.begin(9600);
-
-    pinMode(redLedPin, OUTPUT);
-    pinMode(greenLedPin, OUTPUT);
-    pinMode(buzzerPin, OUTPUT);
-    pinMode(gasSensorPin, INPUT);
-}
-
-void loop() {
-    int gasValue = analogRead(gasSensorPin);
-
-    Serial.print("gas_value = ");
-    Serial.println(gasValue);
-
-    if (gasValue > gasThreshold) {
-        Serial.println("WARNING GAS LEAK");
-        tone(buzzerPin, 1000);
-        digitalWrite(redLedPin, HIGH);
-        digitalWrite(greenLedPin, LOW);
-    } else {
-        noTone(buzzerPin);
-        digitalWrite(redLedPin, LOW);
-        digitalWrite(greenLedPin, HIGH);
-    }
-
-    delay(1000);
-} analogRead(gasSensorPin);
-
-    Serial.print("gas_value = ");
-    Serial.println(gasValue);
-
-    if (gasValue > gasThreshold) {
-        Serial.println("WARNING GAS LEAK");
-        tone(buzzerPin, 1000);
-        digitalWrite(redLedPin, HIGH);
-        digitalWrite(greenLedPin, LOW);
-    } else {
-        noTone(buzzerPin);
-        digitalWrite(redLedPin, LOW);
-        digitalWrite(greenLedPin, HIGH);
-    }
-
-    delay(1000);
+  delay(500); 
 }
